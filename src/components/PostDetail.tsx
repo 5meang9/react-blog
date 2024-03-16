9379f67bfe34173a184aad7e26d254296ec2e835
5meang9 <uiop3724@daum.net>
2024-03-16 22:02:04 +0900

    1. 게시글, 프로필, 홈페이지 대문 레이아웃 진행 2. Header, Footer 컴포넌트로 변경 3. 프로필에 내 게시글 보이도록 변경

---
+++ src/components/PostDetail.tsx
@@ -0,0 +1,29 @@
+export default function PostDetail(){
+  return(
+    <>
+      <div className="post__detail">
+        <div className="post__box">
+          <div className="post__title">블로그 페이지 작성부분</div>
+          <div className="post__profile-box">
+            <div className="post__profile" />
+            <div className="post__author-name">오지원</div>
+            <div className="post__date">2024.03.16 토요일</div>
+          </div>
+          <div className="post__utils-box">
+            <div className="post__delete">삭제</div>
+            <div className="post__edit">수정</div>
+          </div>
+          <div className="post__text">
+            만나서 반가워요
+            만나서 반가워요
+            만나서 반가워요
+            만나서 반가워요
+            만나서 반가워요
+            만나서 반가워요
+            만나서 반가워요
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
