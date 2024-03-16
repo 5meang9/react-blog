3d2e24cea395feeb3f82c6ec62f81b7b2c697980
5meang9 <uiop3724@daum.net>
2024-03-16 18:48:49 +0900

    1. 프로젝트 구조 생성 2. 레이아웃 작업 및 css 수정

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,15 +1,25 @@
 import { Route, Routes, Navigate} from 'react-router-dom'
+import Home from '../pages/home';
+import PostList from '../pages/posts';
+import PostDetail from '../pages/posts/detail';
+import PostNew from '../pages/posts/new';
+import PostEdit from '../pages/posts/edit';
+import ProfilePage from '../pages/profile';
+import LoginPage from '../pages/login';
+import SignupPage from '../pages/signup';
 
 export default function Router() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<h1>Home Page</h1>} />
-        <Route path='/posts' element={<h1>Post List Page</h1>} />
-        <Route path='/posts/:id' element={<h1>Post Detail Page</h1>} />
-        <Route path='/posts/new' element={<h1>Post New Page</h1>} />
-        <Route path='/posts/edit/:id' element={<h1>Post Edit Page</h1>} />
-        <Route path='/profile' element={<h1>Profile Page</h1>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/posts' element={<PostList />} />
+        <Route path='/posts/:id' element={<PostDetail/>} />
+        <Route path='/posts/new' element={<PostNew />} />
+        <Route path='/posts/edit/:id' element={<PostEdit />} />
+        <Route path='/profile' element={<ProfilePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignupPage />} />
         {/* 정의하지 않은 path 를 입력했을 때 디폴트 루트 페이지 */}
         <Route path='*' element={<Navigate replace to="/" />} />
       </Routes>
