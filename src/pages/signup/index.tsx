ce17fa6db903c990a5131238fb8e60f08fb0dfe9
5meang9 <uiop3724@daum.net>
2024-03-16 23:33:58 +0900

    1. firebase 설치 및 로그인, 회원가입 페이지 레이아웃 완료 2. 인증받지 않은 사용자 접근 시, 로그인 페이지 노출되도록 라우터 수정

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,3 +1,13 @@
+import Footer from "components/Footer";
+import Header from "components/Header";
+import SignupForm from "components/SignupForm";
+
 export default function SignupPage(){
-  return <h2>Signup</h2>;
+  return (
+    <>
+      <Header />
+      <SignupForm />
+      <Footer />
+    </>
+  );
 }
